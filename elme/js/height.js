4c1d4dd9b46e58143dc6b1e464a587e4e39c0a59
shady-chen <710254315@qq.com>
2017-06-21 13:23:05 +0800

    elme

---
+++ elme/js/height.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+console.log(Date.now());
+$('.btn').click(function(){
+	$('.swiper-container1').fadeOut(200);
+	$('#main').show();
+});
+
+
+})
+
+
+/***************Maps****/
+
